c24a63cc114ddc1ddaa3ecbee896ad77d64587f1
Chris <chris.beda@gmail.com>
2022-11-19 19:34:20 -0800

    Created a RecipeEntry controller. Added a workstationname xml binding to it to get the workstation name that is required to make the recipe.

---
+++ Quartz/Source/Controllers/Crafting/XUiC_RecipeEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartz
+{
+    public class XUiC_RecipeEntry : global::XUiC_RecipeEntry
+    {
+
+        public override bool GetBindingValue(ref string value, string bindingName)
+        {
+            switch(bindingName)
+            {
+                case "workstationname":
+                    value = Recipe != null ? Localization.Get(Recipe.craftingArea): "";
+                    return true;
+                default:
+                    return base.GetBindingValue(ref value, bindingName);
+            }
+        }
+    }
+}
