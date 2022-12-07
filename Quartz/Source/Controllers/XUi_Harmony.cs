170c399975636241762edc93f4cbce3dc925e7cd
Chris <chris.beda@gmail.com>
2022-12-07 13:23:39 -0800

    First experimental test of loading fonts and using them.

---
+++ Quartz/Source/Controllers/XUi_Harmony.cs
@@ -0,0 +1,66 @@
+﻿/*Copyright 2022 Christopher Beda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+using HarmonyLib;
+using QuartzOverhaul.Extensions;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[HarmonyPatch(typeof(XUi))]
+public static class XUiPatch
+{
+
+    [HarmonyPrefix]
+    [HarmonyPatch("GetUIFontByName")]
+    public static bool GetUIFontByName(XUi __instance, ref NGUIFont __result, string _name, bool _showWarning = true)
+    {
+
+        __result = __instance.GetNGUIFontByName(_name);
+
+        if (__result == null && _showWarning)
+        {
+            Log.Warning("XUi font not found: " + _name + ", from: " + StackTraceUtility.ExtractStackTrace());
+        }
+
+        return false;
+
+        //if (__instance.NGUIFonts.Length == 1)
+        //{
+        //    NGUIFont original = __instance.NGUIFonts[0];
+        //    NGUIFont ariel = new NGUIFont();
+
+        //    ariel.name = "Ariel";
+        //    Font arielFont = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        //    ariel.dynamicFont= arielFont;
+
+        //    __instance.NGUIFonts = new NGUIFont[] { original, ariel };
+        //}
+
+        //for (int i = 0; i < __instance.NGUIFonts.Length; i++)
+        //{
+        //    if (__instance.NGUIFonts[i].name.EqualsCaseInsensitive(_name) || __instance.NGUIFonts[i].spriteName.EqualsCaseInsensitive(_name))
+        //    {
+        //        __result = __instance.NGUIFonts[i];
+        //        return false;
+        //    }
+        //}
+        //if (_showWarning)
+        //{
+        //    Log.Warning("XUi font not found: " + _name + ", from: " + StackTraceUtility.ExtractStackTrace());
+        //}
+
+        //return false;
+    }
+}
--- Quartz/Source/Overhaul/Controllers/Extensions/XuiExtensions.cs
+++ Quartz/Source/Overhaul/Controllers/Extensions/XuiExtensions.cs
@@ -12,10 +12,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
+using Quartz;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.TextCore.LowLevel;
+
 namespace QuartzOverhaul.Extensions
 {
     public static class XuiExtensions
     {
+
+        private static Dictionary<string, NGUIFont> fonts = new Dictionary<string, NGUIFont>();
 
         public static T GetChildByInterface<T>(this XUi xui) where T : class
         {
@@ -32,6 +40,111 @@ namespace QuartzOverhaul.Extensions
             }
 
             return null;
+        }
+
+        public static NGUIFont GetNGUIFontByName(this XUi xui, string name)
+        {
+
+            if (fonts.Count == 0)
+            {
+                foreach (NGUIFont nguiFont in xui.NGUIFonts)
+                {
+                    fonts.Add(nguiFont.name, nguiFont);
+
+                    if (nguiFont.name != nguiFont.spriteName)
+                    {
+                        fonts.Add(nguiFont.spriteName, nguiFont);
+                    }
+                }
+            }
+
+            NGUIFont font;
+
+            if (fonts.TryGetValue(name, out font))
+            {
+                return font;
+            }
+
+            if (name.Contains("@modfolder("))
+            {
+                Font loadedFont = DataLoader.LoadAsset<Font>(name);
+
+                if(loadedFont != null)
+                {
+                    font = ScriptableObject.CreateInstance<NGUIFont>();
+                    font.name = name;
+                    font.dynamicFont = loadedFont;
+
+                    fonts.Add(name, font);
+
+                    return font;
+                } 
+                //else 
+                //{
+                //    string path = ModManager.PatchModPathString(name);
+                //    if(path != null && File.Exists(path))
+                //    {
+                //        FontEngine.LoadFontFace(path);
+                //        loadedFont = new Font("file://" + name);
+
+                //        foreach (string fontname in loadedFont.fontNames)
+                //        {
+                //            Logging.Inform("Font Name = " + fontname);
+                //        }
+
+                //        foreach (CharacterInfo characterInfo in loadedFont.characterInfo)
+                //        {
+                //            Logging.Inform("Character Info = " + characterInfo.ToString());
+                //        }
+
+                //        if (loadedFont != null)
+                //        {
+                //            font = ScriptableObject.CreateInstance<NGUIFont>();
+                //            font.name = name;
+                //            font.dynamicFont = loadedFont;
+
+                //            fonts.Add(name, font);
+
+                //            return font;
+                //        }
+                //    }
+                //}
+            }
+            else
+            {
+                return TryLoadOSInstalledFont(name);
+            }
+
+            return null;
+        }
+
+        public static NGUIFont TryLoadOSInstalledFont(string fontName)
+        {
+            NGUIFont font = null;
+            string[] osFonts = Font.GetOSInstalledFontNames();
+            Font loadedFont = null;
+
+            foreach (string osFont in osFonts)
+            { 
+                if (osFont == fontName)
+                {
+                    loadedFont = Font.CreateDynamicFontFromOSFont(osFont, 30);
+                }
+            }
+
+            if (loadedFont != null)
+            {
+                font = ScriptableObject.CreateInstance<NGUIFont>();
+                font.name = fontName;
+                font.dynamicFont = loadedFont;
+
+                fonts.Add(fontName, font);
+
+                return font;
+            }
+
+            return null;
+
         }
     }
 }
