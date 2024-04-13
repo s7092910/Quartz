0511d963a73e1fc222e3c2f679f9699dc2357610
Christopher Beda <chris.beda@gmail.com>
2024-04-12 17:29:05 -0700

    Code cleanup

--- Quartz/Source/Controllers/Map/XUiC_MiniMap.cs
+++ Quartz/Source/Controllers/Map/XUiC_MiniMap.cs
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.*/
 
 using Audio;
-using InControl;
 using Quartz.Inputs;
 using Quartz.Map;
 using Quartz.Settings;
