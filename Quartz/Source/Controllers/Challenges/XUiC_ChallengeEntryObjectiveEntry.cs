de7abfcc0f9d2cd005a33a530f6c5a4a05df650d
Christopher Beda <chris.beda@gmail.com>
2024-09-03 13:55:39 -0700

    Added missing license heading

--- Quartz/Source/Controllers/Challenges/XUiC_ChallengeEntryObjectiveEntry.cs
+++ Quartz/Source/Controllers/Challenges/XUiC_ChallengeEntryObjectiveEntry.cs
@@ -13,12 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.*/
 
 using Challenges;
-using Quartz.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quartz
 {
