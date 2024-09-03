39b1b8a0530f2f2274728b7f3b79c2e60c4d3e47
Christopher Beda <chris.beda@gmail.com>
2024-09-02 20:32:32 -0700

    Added a max level check on {playerxptonextlevel} to return a localization text saying Max Level instead of the previous level's xp amount.

---
+++ Quartz/Source/Controllers/CharacterFrame/XUiC_CharacterFrameWindow_Harmony.cs
@@ -0,0 +1,34 @@
+﻿/*Copyright 2024 Christopher Beda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+using HarmonyLib;
+
+namespace Quartz
+{
+
+    [HarmonyPatch(typeof(XUiC_CharacterFrameWindow))]
+    public static class XUiC_CharacterFrameWindowPatch
+    {
+
+        [HarmonyPostfix]
+        [HarmonyPatch("GetBindingValue")]
+        public static void GetBindingValue(XUiC_CharacterFrameWindow __instance, ref string value, string bindingName)
+        {
+            if (__instance.player != null && bindingName == "playerxptonextlevel" && XUiM_Player.GetLevel(__instance.player) == Progression.MaxLevel)
+            {
+                value = Localization.Get("quartzMaxLevel");
+            }
+        }
+    }
+}
