a697dd8a2d119d541618c395c9a5c63eafe1e949
Christopher Beda <chris.beda@gmail.com>
2024-09-02 20:33:29 -0700

    Added XUiC_ChallengeEntryObjectiveEntry to allow more than 3 objectives in the ChallengeEntryDescriptionWindow.

---
+++ Quartz/Source/Controllers/Challenges/XUiC_ChallengeEntryDescriptionWindow_Harmony.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[HarmonyPatch(typeof(XUiC_ChallengeEntryDescriptionWindow))]
+public static class XUiC_ChallengeEntryDescriptionWindowPatch
+{
+
+    [HarmonyPostfix]
+    [HarmonyPatch("SetChallenge")]
+    public static void SetChallenge(global::XUiC_ChallengeEntryDescriptionWindow __instance, XUiC_ChallengeEntry challengeEntry)
+    {
+        if (__instance is Quartz.XUiC_ChallengeEntryDescriptionWindow descriptionWindow)
+        {
+            descriptionWindow.SetObjectives();
+        }
+    }
+}
